c6e6f185642de0e1cf6d3043050481f93e7cde6b
RikuH <riku_hei@hotmail.com>
2020-03-07 14:29:23 +0200

    Player
    Player moving, Pathfinding and animation

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMove : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    const int Left_Mouse_Button = 0;
+    NavMeshAgent agent;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        targetPosition = this.transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            setTargetPosition();
+            movePlayer();
+        }
+
+    }
+
+    void setTargetPosition()
+    {
+        Plane plane = new Plane(Vector3.up, this.transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float point = 0;
+
+        if(plane.Raycast(ray, out point))
+        {
+            targetPosition = ray.GetPoint(point);
+        }
+    }
+
+    void movePlayer()
+    {
+        agent.SetDestination(targetPosition);
+
+    }
+}
